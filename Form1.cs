eeb4317328f1d0be70df9900ee26d44b2da82f16
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-22 08:35:32 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiTvrijeme
+{
+    public partial class Form1 : Form
+    {
+        DateTime start = DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();    
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {           
+            toolStripStatusLabel1.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - start;
+            toolStripStatusLabel2.Text = "Proteklo: " + proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+        }
+    }
+}
